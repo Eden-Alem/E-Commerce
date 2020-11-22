5fcd3c3a51f4ef2b721b2ac08245660d7850c140
Eden Berhan Alem <edenberhanalem@gmail.com>
2020-11-22 19:21:21 +0300

    Updates

---
+++ precache-manifest.02cd75cdaadaf91c01b9a2aa0f34698c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eae115d05cd7754d537b4d9f520924c9",
+    "url": "/E-Commerce/index.html"
+  },
+  {
+    "revision": "dfbd6d6fcb38b6dc6c1c",
+    "url": "/E-Commerce/static/css/main.9fabc105.chunk.css"
+  },
+  {
+    "revision": "eb8999171619340a7b5c",
+    "url": "/E-Commerce/static/js/2.fa707cbf.chunk.js"
+  },
+  {
+    "revision": "73aa81e0f73e9e0a2f1a185591abe939",
+    "url": "/E-Commerce/static/js/2.fa707cbf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dfbd6d6fcb38b6dc6c1c",
+    "url": "/E-Commerce/static/js/main.3b1dd5c8.chunk.js"
+  },
+  {
+    "revision": "ea39d67f21e44b3ae702",
+    "url": "/E-Commerce/static/js/runtime-main.38732c37.js"
+  }
+]);
